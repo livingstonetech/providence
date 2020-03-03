925877e35bb1ea396a5e3917ab17b1aff7ef21b7
Pooja Kulkarni <poojakulkarni562@gmail.com>
2020-03-03 09:47:58 +0530

    Removed unnecessary error statements from parser and added conditions to check if file/dir exists in eventRecorder

--- dispatcher/parser.go
+++ dispatcher/parser.go
@@ -3,24 +3,13 @@ package dispatcher
 import (
 	"encoding/json"
 	"encoding/xml"
-	"fmt"
 )
-
-type parseError string
-
-func (p parseError) Error() string {
-	return fmt.Sprintf(string(p))
-}
 
 // ParseJSON called by dispatcher to parse given struct to json and return error, if any
 func ParseJSON(logs interface{}) ([]byte, error) {
 	parsedData, err := json.Marshal(logs)
 	if err != nil {
-		_, ok := err.(*json.UnsupportedTypeError)
-		if ok {
-			return []byte{}, parseError("Tried to marshal invalid type")
-		}
-		return []byte{}, parseError("Couldn't parse into json!")
+		return []byte{}, err
 	}
 	return parsedData, nil
 }
@@ -29,11 +18,7 @@ func ParseJSON(logs interface{}) ([]byte, error) {
 func ParseXML(logs interface{}) ([]byte, error) {
 	parsedData, err := xml.Marshal(logs)
 	if err != nil {
-		_, ok := err.(*xml.UnsupportedTypeError)
-		if ok {
-			return []byte{}, parseError("Tried to marshal invalid type!")
-		}
-		return []byte{}, parseError("Couldn't parse into XML!")
+		return []byte{}, err
 	}
 	return parsedData, nil
 }
