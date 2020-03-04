aeab4b97a7dfab2282b4cefa33395a58ad60017c
Akshay Shah <ans.akshays@gmail.com>
2020-03-04 23:22:04 +0530

    feat(dispatcher): restructured dispatcher

--- transformer/transformerLinux.go
+++ transformer/transformerLinux.go
@@ -3,23 +3,37 @@
 package transformer
 
 import (
+	"fmt"
 	"github.com/livingstonetech/providence/dispatcher"
 	"github.com/spf13/viper"
 )
 
 type Transformer struct {
 	Config		*viper.Viper
-	Dispatcher	*dispatcher.Dispatcher
+	Dispatchers	[]*dispatcher.Dispatcher
 }
 
 func CreateTransformer(config *viper.Viper) *Transformer {
-	return &Transformer{Config: config, Dispatcher: dispatcher.CreateDispatcher(config)}
+	t := Transformer{
+		Config:      config,
+		Dispatchers: make([]*dispatcher.Dispatcher, 0),
+	}
+	dispatchers := make([]*dispatcher.Dispatcher, 0)
+	for k, _ := range config.GetStringMap("dispatch") {
+		d := dispatcher.CreateDispatcher(config.GetStringMap(fmt.Sprintf("dispatch.%v", k)))
+		dispatchers = append(dispatchers, d)
+	}
+	t.Dispatchers = dispatchers
+	return &t
 }
 
-func (t Transformer) filter() {
-
+func (t Transformer) filter(event interface{}) bool {
+	return true
 }
 
-func (t Transformer) Transform() {
-	
+func (t Transformer) Transform(event interface{}) {
+	errChan := make(chan error)
+	for _, d := range t.Dispatchers {
+		go d.Dispatch(event, errChan)
+	}
 }
